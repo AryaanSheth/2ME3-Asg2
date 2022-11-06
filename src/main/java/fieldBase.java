bcc5a86091f902318f1d6a2ba764870901be83ad
Aryaan Sheth <avhsheth03@gmail.com>
2022-11-06 16:09:00 -0500

    implement encryption

---
+++ src/main/java/fieldBase.java
@@ -0,0 +1,4 @@
+public class fieldBase extends homeBase{
+    // subclass of homeBase,
+
+}
--- src/main/java/homebasetest.java
+++ src/main/java/homebasetest.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class homebasetest {
+public class homebasetest {
+    public static void main(String[] args) {
+        homeBase base = homeBase.getInstance();
+        System.out.println(base.encrypt("Hello World", 5));
+        System.out.println(base.decrypt("Mjqqt%\\twqi", 5));
+    }
 }
