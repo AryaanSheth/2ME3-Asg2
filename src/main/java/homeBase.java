91160bb9b4a5c6452359964fb3414801df8d8a49
Aryaan Sheth <avhsheth03@gmail.com>
2022-11-08 19:47:09 -0500

    homeBase update

--- src/main/java/homeBase.java
+++ src/main/java/homeBase.java
@@ -1,39 +1,58 @@
-import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
 
-// Singleton Obj for HomeBase
-public class homeBase implements encryption {
-    private static homeBase instance = null;
-    private int key;
+/**
+ * The type Home base -- Singleton
+ */
+public class homeBase<T> extends encryption{
+    private static homeBase instance;
+    private static int key;
+    private static char encodingProtocol;
+    private static final ArrayList<fieldBaseInterface> fieldBases = new ArrayList<>();
 
-    // Singleton Instance for HomeBase Class
-    public static homeBase getInstance() {
+
+    static homeBase getInstance() {
         if (instance == null) {
-            System.out.println("Creating new instance");
+            System.out.println("Creating New Home Base");
             instance = new homeBase();
+            key = 0; // default key
         }
         return instance;
     }
 
-    // encryption.java, bitshifts the text by the key
-    @Override
-    public String encrypt(@NotNull String text, int key) {
-        this.key = key;
-        // bit shift the text by the key value
-        StringBuilder encryptedText = new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-            encryptedText.append((char) (text.charAt(i) + key));
+
+    public static int getKey() {
+        return key;
+    }
+    static void setKey(int key){homeBase.key = key;}
+
+
+    public static void addFieldBase(fieldBaseInterface field) {fieldBases.add(field);} // subscribe
+    public static void removeFieldBase(fieldBaseInterface field) {fieldBases.remove(field);} // unsubscribe
+    public int getFieldBases() {return fieldBases.size();}
+
+
+
+    // send a message to either a specific field base or a specific spy using the generic type <T>
+    public void sendMessage(T target, String message) throws Exception {
+        // send message to a specific instance of a field base
+        if (target instanceof fieldBaseInterface){
+            System.out.println("Sending message to field base");
+            ((fieldBaseInterface) target).recieveMessage(encrypt(message, key));
         }
-        return encryptedText.toString();
+        // send message to a specific instance of a spy
+        else if (target instanceof spyInterface){
+            System.out.println("Sending message to spy");
+            ((spyInterface) target).recieveMessage(encrypt(message, key));
+        }
+        else{
+            throw new Exception("Invalid target");
+        }
     }
 
-    @Override
-    public String decrypt(String text, int key) {
-        this.key = key;
-        // bit shift the text by the key value
-        StringBuilder decryptedText = new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-            decryptedText.append((char) (text.charAt(i) - key));
-        }
-        return decryptedText.toString();
+    public void recieveMessage(String message) {
+        System.out.println("Recieved message: " + decrypt(message, key));
     }
+
+
+
 }
